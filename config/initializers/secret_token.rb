ca9424d4c32cefcd9eb8271d1a61425bf0029fa5
Deepwinter <deepwinter@winterroot.net>
2014-05-20 13:12:09 -0700

    Generate rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocoApi::Application.config.secret_token = '35b0cf4bfa89fc00d2627ac8a0ff0833a079822bacb2c159fd1afb9374fc137c4f0fe354ed47b34eca506ecbbad13989d632fd68f862081180108402449d1aa2'
