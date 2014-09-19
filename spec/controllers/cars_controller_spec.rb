31b577897caa52b1a4eebc1163b89561443cb422
Ariel Fogel <fogeltine@gmail.com>
2014-09-19 14:31:02 -0700

    Add tests to cars controller spec for cars#create

--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -15,4 +15,17 @@ describe CarsController do
       expect(response.status).to equal(200)
     end
   end
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      it 'saves the new car to the database' do
+        expect{
+          post :create, car: FactoryGirl.attributes_for(:car)
+        }.to change(Car, :count).by(1)
+      end
+      it 'redirects to cars#show' do
+        post :create, car: FactoryGirl.attributes_for(:car)
+        expect(response).to redirect_to car_path(assigns(:car))
+      end
+    end
+  end
 end
