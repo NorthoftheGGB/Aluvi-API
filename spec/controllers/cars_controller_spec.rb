6bcd7532ea5188cc5f19110aa9c6a07e9ef4fe87
Ariel Fogel <fogeltine@gmail.com>
2014-09-19 13:55:26 -0700

    Add tests for cars#index

--- spec/controllers/cars_controller_spec.rb
+++ spec/controllers/cars_controller_spec.rb
@@ -1,3 +1,12 @@
 describe CarsController do
-
+  context "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to equal(200)
+    end
+    it 'assigns an instance variable cars' do
+      get :index
+      expect(assigns(:cars)).to_not be(nil)
+    end
+  end
 end
