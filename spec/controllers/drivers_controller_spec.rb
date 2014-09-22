1740cfc0214cc30f42ab6b7448e424b62c8c2959
Ariel Fogel <fogeltine@gmail.com>
2014-09-22 16:58:50 -0700

    Add controller test for driver#new

--- spec/controllers/drivers_controller_spec.rb
+++ spec/controllers/drivers_controller_spec.rb
@@ -11,4 +11,11 @@ describe DriversController do
       expect(assigns(:drivers)).to_not be(nil)
     end
   end
+
+  describe 'GET #new' do
+    it 'gets a successful response' do
+      get :new
+      expect(response.status).to equal(200)
+    end
+  end
 end
