557b55171e7a30668b68d9b9e11dee75f9aa0315
Ariel Fogel <fogeltine@gmail.com>
2014-09-22 16:56:30 -0700

    Add controller tests for driver#index

--- spec/controllers/drivers_controller_spec.rb
+++ spec/controllers/drivers_controller_spec.rb
@@ -1,2 +1,14 @@
 describe DriversController do
+  let(:driver) {FactoryGirl.create(:driver)}
+
+  describe "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to equal(200)
+    end
+    it 'assigns an instance variable drivers' do
+      get :index
+      expect(assigns(:drivers)).to_not be(nil)
+    end
+  end
 end
