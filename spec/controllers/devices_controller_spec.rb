e942b2ececfb1439f2891ae11730494274cb1ede
Ariel Fogel <fogeltine@gmail.com>
2014-09-22 15:07:25 -0700

    Add tests to devices_controller_spec for device#index.

--- spec/controllers/devices_controller_spec.rb
+++ spec/controllers/devices_controller_spec.rb
@@ -1,2 +1,14 @@
 describe DevicesController do
+  let(:device) {FactoryGirl.create(:device)}
+
+  describe "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to equal(200)
+    end
+    it 'assigns an instance variable devices' do
+      get :index
+      expect(assigns(:devices)).to_not be(nil)
+    end
+  end
 end
