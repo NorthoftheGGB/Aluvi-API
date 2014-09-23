d527ed0303e34d9bde4d20217924005b31ebe2a5
Ariel Fogel <fogeltine@gmail.com>
2014-09-23 14:25:15 -0700

    Add controller tests for fares#index

--- spec/controllers/fares_controller_spec.rb
+++ spec/controllers/fares_controller_spec.rb
@@ -1,2 +1,14 @@
 describe FaresController do
+  let(:fare) {FactoryGirl.create(:fare)}
+
+  describe "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to equal(200)
+    end
+    it 'assigns an instance variable fares' do
+      get :index
+      expect(assigns(:fares)).to_not be(nil)
+    end
+  end
 end
