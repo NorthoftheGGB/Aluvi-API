cc2e5d8f529df406abbbd82ca8d46c76a3eaa7e6
Ariel Fogel <fogeltine@gmail.com>
2014-09-23 16:34:10 -0700

    Add controller test in spec for riders#index

--- spec/controllers/riders_controller_spec.rb
+++ spec/controllers/riders_controller_spec.rb
@@ -1,2 +1,14 @@
 describe RidersController do
+   let(:rider) {FactoryGirl.create(:rider)}
+
+  describe "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to equal(200)
+    end
+    it 'assigns an instance variable riders' do
+      get :index
+      expect(assigns(:riders)).to_not be(nil)
+    end
+  end
 end
