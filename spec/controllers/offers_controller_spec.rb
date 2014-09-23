21075e5e6a5e3e030ee0969edd357bd047aede2a
Ariel Fogel <fogeltine@gmail.com>
2014-09-23 15:20:02 -0700

    Add controller tests for offers#index

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -1,2 +1,14 @@
 describe OffersController do
+  let(:offer) {FactoryGirl.create(:offer)}
+
+  describe "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to equal(200)
+    end
+    it 'assigns an instance variable offers' do
+      get :index
+      expect(assigns(:offers)).to_not be(nil)
+    end
+  end
 end
