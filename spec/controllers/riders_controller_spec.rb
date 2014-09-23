19544a73e428ef6ec8c7a10a64ef634f4f480524
Ariel Fogel <fogeltine@gmail.com>
2014-09-23 16:35:25 -0700

    Add controller test in spec for riders#new.

--- spec/controllers/riders_controller_spec.rb
+++ spec/controllers/riders_controller_spec.rb
@@ -1,5 +1,5 @@
 describe RidersController do
-   let(:rider) {FactoryGirl.create(:rider)}
+  let(:rider) {FactoryGirl.create(:rider)}
 
   describe "GET #index" do
     it 'gets a successful response' do
@@ -9,6 +9,13 @@ describe RidersController do
     it 'assigns an instance variable riders' do
       get :index
       expect(assigns(:riders)).to_not be(nil)
+    end
+  end
+
+  describe 'GET #new' do
+    it 'gets a successful response' do
+      get :new
+      expect(response.status).to equal(200)
     end
   end
 end
