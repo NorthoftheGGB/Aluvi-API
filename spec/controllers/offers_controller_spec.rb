6dc2bac16bc181362f1c83afd5743e2782a1f389
Ariel Fogel <fogeltine@gmail.com>
2014-09-23 15:21:25 -0700

    Add controller test for offers#new

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -11,4 +11,11 @@ describe OffersController do
       expect(assigns(:offers)).to_not be(nil)
     end
   end
+
+  describe 'GET #new' do
+    it 'gets a successful response' do
+      get :new
+      expect(response.status).to equal(200)
+    end
+  end
 end
