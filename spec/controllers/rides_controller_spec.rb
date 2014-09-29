74d3eaa54204ffaf6d52aaf828bf9a23856fc98a
Ariel Fogel <fogeltine@gmail.com>
2014-09-29 18:14:56 -0400

    Add controller spec tests for rides#index

--- spec/controllers/rides_controller_spec.rb
+++ spec/controllers/rides_controller_spec.rb
@@ -1,3 +1,14 @@
 describe RidesController do
+  let(:ride) {FactoryGirl.create(:on_demand_ride)}
 
+  describe 'GET #index' do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to be(200)
+    end
+    it 'assigns a @rides variable' do
+      get :index
+      expect(assigns(:rides)).to_not be(nil)
+    end
+  end
 end
