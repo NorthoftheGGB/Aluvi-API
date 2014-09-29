77fec290db09936a251d54c3341a0f5d5d733455
Ariel Fogel <fogeltine@gmail.com>
2014-09-29 09:55:52 -0400

    Add controller spec for payments#index

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,2 +1,13 @@
 describe PaymentsController do
+  let(:payment) {FactoryGirl.create(:payment)}
+  describe 'GET #index' do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it 'assigns an instance variable payments' do
+      get :index
+      expect(assigns(:payments)).to_not be(nil)
+    end
+  end
 end
