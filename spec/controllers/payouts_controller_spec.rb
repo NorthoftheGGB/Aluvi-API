2add80913b34abcd9b835105b6d88f2ff6706f15
Ariel Fogel <fogeltine@gmail.com>
2014-09-29 16:32:42 -0400

    Create controller spec tests for payouts#index

--- spec/controllers/payouts_controller_spec.rb
+++ spec/controllers/payouts_controller_spec.rb
@@ -1,2 +1,14 @@
 describe PayoutsController do
+  let(:payout) {FactoryGirl.create(:payout)}
+
+  describe "GET #index" do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to be(200)
+    end
+    it 'assigns a variable @payouts' do
+      get :index
+      expect(assigns(:payouts)).to_not be(nil)
+    end
+  end
 end
