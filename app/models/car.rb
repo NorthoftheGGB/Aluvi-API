dcfb8b986c12da14dd4048fb53b7caa40e64976f
Ariel Fogel <fogeltine@gmail.com>
2014-10-12 16:55:42 -0700

    Add color to Car model and show page

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,9 @@
 class Car < ActiveRecord::Base
 	belongs_to :driver, inverse_of: :cars
 	has_many :fares, inverse_of: :car
-	attr_accessible :license_plate, :make, :model, :state, :year, :car_photo
-	has_attached_file :car_photo,  :styles => { :thumb => "160x66#" }, :default_url => "/images/missing.png", :storage => :s3 
-	validates_attachment_content_type :car_photo, :content_type => /\Aimage\/.*\Z/ 
+	attr_accessible :license_plate, :make, :model, :state, :year, :car_photo, :color
+	has_attached_file :car_photo,  :styles => { :thumb => "160x66#" }, :default_url => "/images/missing.png", :storage => :s3
+	validates_attachment_content_type :car_photo, :content_type => /\Aimage\/.*\Z/
 
 	def summary
     summary = ''
