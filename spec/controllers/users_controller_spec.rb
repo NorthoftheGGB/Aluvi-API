64badbae75262270430b6a10f10868c5d4da069a
Ariel Fogel <fogeltine@gmail.com>
2014-10-12 18:14:40 -0700

    Add controller tests spec for users#create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,5 @@
 describe UsersController do
-  let(:user) {FactoryGirl.create(:rider)}
+  let(:user) {FactoryGirl.create(:user)}
 
   describe 'GET #index' do
     it 'gets a successful response' do
@@ -18,4 +18,28 @@ describe UsersController do
       expect(response.status).to eq(200)
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid parameters' do
+      it 'saves a new user to the database' do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to change(User, :count).by(1)
+      end
+      context 'if user is not interested in being a driver' do
+        it 'redirects to users#show' do
+          post :create, user: FactoryGirl.attributes_for(:user)
+          expect(response).to redirect_to user_path(assigns(:user))
+        end
+      end
+      context 'if user is interested in being a driver' do
+        it 'redirects to drivers#edit' do
+          post :create, user: FactoryGirl.attributes_for(:user_interested_in_being_driver)
+          expect(response).to redirect_to edit_driver_path(assigns(:user).as_driver)
+        end
+      end
+    end
+  end
+
+
 end
