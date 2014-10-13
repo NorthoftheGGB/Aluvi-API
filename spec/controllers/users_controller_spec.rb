37447ce6c5100b38a269c17ecf6465cd498817dd
Ariel Fogel <fogeltine@gmail.com>
2014-10-12 17:46:17 -0700

    Add controller tests spec for user#index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,3 +1,14 @@
 describe UsersController do
+  let(:user) {FactoryGirl.create(:rider)}
 
+  describe 'GET #index' do
+    it 'gets a successful response' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+    it 'assigns an instance variable users' do
+      get :index
+      expect(assigns(:users)).to_not be(nil)
+    end
+  end
 end
