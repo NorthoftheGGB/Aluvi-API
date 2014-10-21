335e4a78d34c45e55de3d4259152a1f3329912f5
Ariel Fogel <fogeltine@gmail.com>
2014-10-21 15:50:59 -0700

    Add feature test for driver accepts a fare.

--- spec/features/ride_spec.rb
+++ spec/features/ride_spec.rb
@@ -6,12 +6,29 @@ feature 'Rides' do
 
   let(:rider) {FactoryGirl.create(:rider)}
   let(:ride) {OnDemandRide.create!(ORIGIN, ORIGIN_PLACE_NAME, DESTINATION, DESTINATION_PLACE_NAME, rider)}
+  let(:fare) {ride.fare}
 
   scenario 'request on demand ride' do
     ride.request!
-    fare = ride.fare
     expect(fare).to_not be(nil)
     expect(fare.meeting_point).to_not be(nil)
     expect(fare.drop_off_point).to_not be(nil)
   end
+
+  scenario 'driver accepts fare' do
+    2.times {FactoryGirl.create(:available_driver)}
+    ride.request!
+    driver = Driver.available_drivers.first
+    driver.offer_fare(fare)
+
+    offer = driver.offers.where(fare_id: fare.id).first
+    expect(offer).to_not be(nil)
+
+    offer.accepted!
+    fare.accepted!(driver)
+    expect(fare.state).to eq("scheduled")
+    expect(offer.state).to eq("accepted")
+  end
+
+
 end
