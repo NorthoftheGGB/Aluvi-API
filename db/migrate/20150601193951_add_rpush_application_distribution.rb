e7ab99812c3a4e49f9f372f07ab01f3e6cfaee78
Debian <admin@ip-172-31-13-10.us-west-2.compute.internal>
2015-07-17 01:00:27 +0000

    Fixed misspelling in a migration

--- db/migrate/20150601193951_add_rpush_application_distribution.rb
+++ db/migrate/20150601193951_add_rpush_application_distribution.rb
@@ -1,7 +1,7 @@
 class AddRpushApplicationDistribution < ActiveRecord::Migration
   def up
 		app = Rpush::Apns::App.new
-		app.name = "com.vocotransporation.aluvi"
+		app.name = "com.vocotransportation.aluvi"
 		app.certificate = File.read("keys/aluvi_distribution_push.pem")
 		app.environment = "production" # APNs environment.
 		app.password = ""
