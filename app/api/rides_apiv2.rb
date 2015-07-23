c9539f93f28754f81690924f3476b375033d49a8
Debian <admin@ip-172-31-41-210.us-west-2.compute.internal>
2015-07-23 06:37:54 +0000

    Updated rider fare cancelled to use ride id rather than requiring fare id - to simplify ticket management on devices

--- app/api/rides_apiv2.rb
+++ app/api/rides_apiv2.rb
@@ -12,5 +12,43 @@ class RidesAPIV2< Grape::API
 			@rides = rider.rides.select('rides.*').where('pickup_time > ?', DateTime.now.beginning_of_day) 
 			@rides
 		end
+
+		desc "Rider cancelled fare"
+		params do
+			requires :ride_id, type: Integer
+		end
+		post :rider_cancelled do
+			authenticate!
+
+			# TODO rider should only be able to cancel their own ride
+			begin
+        ride = current_user.as_rider.rides.where(ride_id: params[:ride_id]).first
+				fare = ride.fare
+        unless ride.nil?
+          unless ride.aborted?
+            ride.abort!
+          end
+          unless ride.fare.nil?
+            unless fare.is_cancelled
+             fare.rider_cancelled!(current_user.as_rider)
+            end
+          end
+        end
+				ok
+      rescue AASM::InvalidTransition => e
+        if(fare.is_cancelled && ride.is_aborted)
+					ok
+				else
+					raise e
+				end
+			rescue ActiveRecord::RecordNotFound => e
+				if fare.nil?
+					ok
+				else
+					raise e
+				end
+			end
+		end
+
 	end
 end
