0419f19eebc90a1bdf375e80da37090aac751d55
Debian <admin@ip-172-31-41-210.us-west-2.compute.internal>
2015-07-24 23:38:44 +0000

    Fixed app identifier

--- db/migrate/20150719230751_add_rpush_application_dev_android.rb
+++ db/migrate/20150719230751_add_rpush_application_dev_android.rb
@@ -1,7 +1,7 @@
 class AddRpushApplicationDevAndroid < ActiveRecord::Migration
   def change
 		app = Rpush::Gcm::App.new
-		app.name = "com.aluvi.android.application"
+		app.name = "com.aluvi.android"
 		app.auth_key = File.read("keys/aluvi_android_alpha.key");
 		app.connections = 1
 		app.save!
