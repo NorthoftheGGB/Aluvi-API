9a01ae5f68b3a17fb11091049b3914f6bd6a32df
Matthew.Xi.Api <matthew.xi@aluviapp.com>
2015-08-01 03:48:21 +0000

    Added unit tests for combined commute request api path

--- spec/api/rides_spec.rb
+++ spec/api/rides_spec.rb
@@ -6,12 +6,29 @@ describe RidesAPI do
 
 	before { RidesAPI.before { env["api.tilt.root"] = "app/views/api" } }
 
-  #before(:all) do
-  # @ride = FactoryGirl.create(:ride)
-  # @fare = FactoryGirl.create(:fare)
-  # @rider = FactoryGirl.create(:rider)
-  # @demo_rider = FactoryGirl.create(:demo_rider)
- # end
+	describe "POST /api/rides/commute" do
+		it "creates new commuter ride requests" do
+      @rider = FactoryGirl.create(:rider)
+      post "/api/rides/commute", {:type => 'commuter',:departure_latitude => 45.5,
+                                  :departure_longitude => -122.3, :departure_place_name => "My House",
+                                  :destination_latitude => 46.5, :destination_longitude => -122.4,
+                                  :destination_place_name => 'My Work', 'departure_pickup_time' => '2014-08-16 7:00:00 -14:00',
+																	:return_pickup_time => '2014-08-16 17:00:00 -14:00' }, {'HTTP_AUTHORIZATION' => encode_credentials(@rider.token)}
+      expect(response.status).to eq(201)
+		end
+	end
+
+	describe "POST /api/rides/commute" do
+		it "creates new commuter ride requests" do
+      @rider = FactoryGirl.create(:rider)
+      post "/api/rides/commute", {:type => 'commuter',:departure_latitude => 45.5,
+                                  :departure_longitude => -122.3, :departure_place_name => "My House",
+                                  :destination_latitude => 46.5, :destination_longitude => -122.4,
+                                  :destination_place_name => 'My Work', 'departure_pickup_time' => '2014-08-16 7:00:00 -14:00',
+																	:return_pickup_time => '2014-08-16 17:00:00 -14:00', :driving => true }, {'HTTP_AUTHORIZATION' => encode_credentials(@rider.token)}
+      expect(response.status).to eq(201)
+		end
+	end
 
 	describe "POST /api/rides/request" do
     it "processes commuter ride request" do
