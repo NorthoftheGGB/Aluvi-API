1f3d601ae34f25af6dd9fdb31e697310f8554b48
Matthew.Xi.Api <matthew.xi@aluviapp.com>
2015-08-12 00:37:49 +0000

    Factory for route with pickup zone

--- spec/api/usersv2_spec.rb
+++ spec/api/usersv2_spec.rb
@@ -72,7 +72,15 @@ describe UsersAPI do
       @rider = FactoryGirl.create(:rider)
 			post "/api/users/support", { :message => 'support message' },  {'HTTP_AUTHORIZATION' => encode_credentials(@rider.token)}
 			expect(response.status).to eq(200)
-			Rails.logger.info response.status.to_s + ':' + response.body
+		end
+	end
+
+
+	describe "POST /api/users/profile" do
+		it "returns success" do
+			@rider = FactoryGirl.create(:rider)
+			post "/api/users/profile", { :first_name => @rider.first_name, :last_name => @rider.last_name, :email => @rider.email, :phone => @rider.phone, :default_card_token => 'asdfasdfasdfasdf', :default_recipient_debit_card_token => 'apsoidfjapwoeijfapwoiefja' },  {'HTTP_AUTHORIZATION' => encode_credentials(@rider.token)}
+			expect(response.status).to eq(200)
 		end
 	end
 
--- spec/factories/route.rb
+++ spec/factories/route.rb
@@ -4,7 +4,10 @@ FactoryGirl.define do
 		destination_place_name "destination"
 		origin "POINT(-88.09483 32.12665)"
 		origin_place_name "origin"
+		pickup_zone_center "POINT(-88.09483 32.12665)"
+		pickup_zone_center_place_name "pickup zone"
 		pickup_time "2014-08-16 7:00:00 -14:00" 
 		return_time "2014-08-16 17:00:00 -14:00"
+		driving false
 	end
 end
