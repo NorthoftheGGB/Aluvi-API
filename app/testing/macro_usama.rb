c492793e9f0ff6f6c1d0d966f3fbbb7a87530ce4
Matthew.Xi.Api <matthew.xi@aluviapp.com>
2015-08-17 02:40:23 +0000

    Usamas macros

--- app/testing/macro_usama.rb
+++ app/testing/macro_usama.rb
@@ -1,7 +1,7 @@
 module Macro
 
 	def self.drive_with_one_rider_scheduled
-		Harness.driver_request "y@y.com"	
+		Harness.driver_request "test_driver@test.com"	
 		Harness.rider_request "r@r.com"	
 		Scheduler.build_commuter_trips
 	end
