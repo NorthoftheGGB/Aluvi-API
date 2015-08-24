c5f4837bd8e9c2aaf376a33d68d36efb0f5d0d8f
Matthew.Xi.Api <matthew.xi@aluviapp.com>
2015-08-24 20:45:14 +0000

    Possibly improved error reporting over api.  Switch to json from html

--- app/api/apiv2.rb
+++ app/api/apiv2.rb
@@ -1,9 +1,12 @@
 require 'grape-swagger'
+require 'api_error_handler'
 
 class APIV2 < Grape::API
 	version 'v2', using: :path
 	format :json
 	content_type :json, "application/json"
+
+  user ApiErrorHandler
 
 	helpers do
 		include VocoApiHelperV2
