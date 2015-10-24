536f809031e9555d22ee44a539cb6742ba2ce378
Debian <admin@ip-172-31-24-234.us-west-2.compute.internal>
2015-10-24 02:06:42 +0000

    Fixed typo

--- app/api/debug_api.rb
+++ app/api/debug_api.rb
@@ -8,18 +8,21 @@ class DebugAPI < Grape::API
 		post :purge do
 			Rails.logger.debug "HELLO"
 			authenticate!
-			current_user.as_rider.rides.active.each do |ride|
-        Rails.logger.debug 'hi'
-        TicketManager.cancel_ride ride
-      end
-      current_user.trips.fulfilled.each do |trip|
-        trip.aborted!
-      end
-      current_user.trips.requested.each do |trip|
-        trip.aborted!
-      end
-			Rails.logger.debug "rides"
-			ok
+			ActiveRecord::Base.transaction do
+				current_user.as_rider.rides.active.each do |ride|
+					Rails.logger.debug 'hi'
+					TicketManager.cancel_ride ride
+				end
+				current_user.trips.fulfilled.each do |trip|
+					trip.aborted!
+				end
+				current_user.trips.requested.each do |trip|
+					trip.aborted!
+				end
+				Rails.logger.debug "rides"
+				ok
+			end
+
 		end
 
     desc "Doesnt do anything yet"
